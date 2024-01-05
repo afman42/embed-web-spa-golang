e3aeb840bd97e27293687949a8ab635ae5329a60
Afif Arman <afifarman50@gmail.com>
2024-01-05 22:37:30 +0700

    add flag and load gile serve

--- embed-file-spa-gin/main.go
+++ embed-file-spa-gin/main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
-	"github.com/gin-contrib/static"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	var port string
+	flag.StringVar(&port, "port", "5050", "port server")
+	flag.Parse()
 	r := gin.Default()
 
-	r.Use(static.Serve("/", static.LocalFile("./web/dist", true)))
+	r.LoadHTMLFiles(filePathJoin("/web/dist/index.html"))
 
-	r.NoRoute(func(c *gin.Context) {
-		c.File("./web/dist/index.html")
+	r.Static("/assets", filePathJoin("/web/dist/assets"))
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
 	})
 
-	r.Run(":5050")
+	r.Run(":" + port)
+}
+
+func filePathJoin(filename string) string {
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return filepath.Join(cwd, filename)
 }
